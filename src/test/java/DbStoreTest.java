af34fc9258e1eb6e5ca97db5719decd4f71d2518
Anton Romanov <a.romanov@ulstu.ru>
2022-06-07 17:01:07 +0400

    fix tests

--- src/test/java/DbStoreTest.java
+++ src/test/java/DbStoreTest.java
@@ -1,5 +1,3 @@
-package ru.ulstu;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
--- src/test/java/FileStoreTest.java
+++ src/test/java/FileStoreTest.java
@@ -1,5 +1,3 @@
-package ru.ulstu;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
